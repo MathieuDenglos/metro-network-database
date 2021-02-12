374ca152a733d9408681ab20c03bff4f4941df2d
Mathieu Denglos <matdenglos@gmail.com>
2021-02-12 21:26:44 +0100

    create documentation for desctruction

--- src/destruction.h
+++ src/destruction.h
@@ -5,7 +5,14 @@
 
 namespace DESTRUCT
 {
+    /**
+     * @brief function to remove all the tables and database.
+     * /!\ CALLING THIS FUNCTION ERASE ALL DATA FOREVER.
+     * Function mainly for test purposes
+     * 
+     * @param stmt Used to communicate with the database
+     */
     void remove_tables(sql::Statement *stmt);
-}
+} // namespace DESTRUCT
 
 #endif //DESTRUCT_H
